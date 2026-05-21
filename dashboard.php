ed6a9db4122d6886a4accb6096553a03c452dd69
Paul De Castro <paulpabahay@gmail.com>
2026-05-21 23:27:01 +0800

    Edit dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,28 +5,49 @@ if (!isset($_SESSION['user_id'])) {
     header("Location: auth/login.php");
     exit();
 }
+
+include 'includes/connection.php';
+
+$user_id = $_SESSION['user_id'];
+
+$count_sql = "SELECT COUNT(*) AS total
+              FROM pokemon
+              WHERE user_id='$user_id'";
+
+$count_result = mysqli_query($conn, $count_sql);
+
+$count_row = mysqli_fetch_assoc($count_result);
+
+$total_pokemon = $count_row['total'];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>Dashboard</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
-<h1>Welcome, <?php echo $_SESSION['username']; ?>!</h1>
+<h1>Welcome,
+<?php echo $_SESSION['username']; ?>!</h1>
 
-<a href="auth/logout.php">Logout</a>
+<hr>
 
-<a href="add-pokemon.php">
-    Add Pokémon
-</a>
+<h2>Total Pokémon:
+<?php echo $total_pokemon; ?></h2>
+
+<br>
+
+<a href="add-pokemon.php">Add Pokémon</a>
 
 <br><br>
 
-<a href="collection.php">
-    View Collection
-</a>
+<a href="collection.php">View Collection</a>
+
+<br><br>
+
+<a href="auth/logout.php">Logout</a>
 
 </body>
 </html>
